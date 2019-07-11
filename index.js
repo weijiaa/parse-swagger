9216f6d10f11c28af380d8a55dce19b2b8cd2e63
weijia <gaiwj@fosun.com>
2019-07-11 19:11:42 +0800

    .

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ const createFile  = require('./lib/create-file');
 const param = process.argv.slice(2);
 const [url, outputPath] = param;
 
+if(!url) {
+  console.log('usage: parse-swagger swagger-path [urls-path]');
+  process.exit(-1);
+}
+
+if(url === '-V' || url === '-v' || url === '-version') {
+  console.log(require('./package.json').version);
+  process.exit(-1);
+}
+
 const parseJSON = (json) => {
   let reslut = {};
 
