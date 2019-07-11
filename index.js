fd00cfd8ef081234395829fe3828e7821720b485
weijia <gaiwj@fosun.com>
2019-07-11 17:50:31 +0800

    .

--- index.js
+++ index.js
@@ -1,73 +1,26 @@
 #!/usr/bin/env node
 
-let fs = require('fs');
-let path = require('path');
-let puppeteer = require('puppeteer');
+const getJSON     = require('./lib/getJSON');
+const createFile  = require('./lib/create-file');
 
-let param = process.argv.slice(2);
-let url = param[0];
-let outputPath = path.resolve(param[1] || '');
+const param = process.argv.slice(2);
+const [url, outputPath] = param;
 
-if(!url) {
-  console.log('usage: parse-swagger [url] [filePath]');
-  return void(0);
-};
+const parseJSON = (json) => {
+  let reslut = {};
 
-(async () => {
-  let browser = await puppeteer.launch();
-  let page = await browser.newPage();
-  await page.goto(url);
-  await page.addScriptTag({url: 'https://cdn.bootcss.com/jquery/3.2.0/jquery.min.js'});
-  await page.waitFor(2000);
-  
-  let reslut = await page.evaluate(function (){
-
-    let apis = [];
-    let resources = $('#resources').children('.resource');
-    resources.each((index, el) => {
-
-      let name = $(el).children('.heading').children('h2').text().split(':')[1].trim();
-      let items = [];
-
-      let apis_el = $(el).children('.endpoints').children('.endpoint');
-      apis_el.each((i, e) => {
-        let item = $(e).children('.operations').children('.operation').children('.heading');
-        let path = item.children('h3').children('.path').text().trim();
-        let note = item.children('.options').children('li').children('a').text().trim();
-        let pathSplit = path.split('/');
-        let len = pathSplit.length;
-        let name = pathSplit[len - 3] + (function (name){return name.charAt(0).toUpperCase() + name.slice(1)})(pathSplit[len - 2]) + (function (name){return name.charAt(0).toUpperCase() + name.slice(1)})(pathSplit[len - 1]);
-        items.push({ name, path, note });
-      });
-
-      apis.push(
-        { name, items }
-      );
-
-    });
-    return apis;
-  });
-
-  await browser.close();
-
-  let outputStr = '';
-  let startTxt = 'export default {\r\n';
-  let endTxt = '}\r\n';
-
-  reslut.map(v => {
-    outputStr += '\r\n  /**  \r\n  *'+ v.name +'\r\n  */\r\n\r\n';
-    v.items.map(item => {
-      outputStr += '  // ' + item.note + '\r\n';
-      outputStr += '  ' + item.name + ': ' + "'" + item.path +"',\r\n\r\n";
+  json.map(m => {
+    m.apis.map(api => {
+      reslut[api.name] = api.path;
     })
   })
 
-  let txt = startTxt + outputStr + endTxt;
-  fs.writeFile(outputPath, txt, 'utf8', err => {
-    if(err) {
-      console.log(outputPath + '不存在！');
-      throw err;
-    }
-  });
+  return reslut;
+}
 
+(async () => {
+  const apis = await getJSON(url);
+  let text = `let json = ${JSON.stringify(apis, null, 2)};\r\nexport default (${parseJSON.toString()})(json)`;
+
+  createFile(outputPath, text);
 })();
--- test.js
+++
@@ -1,7 +0,0 @@
-let fs = require('fs');
-let path = require('path');
-let currentPath = require('./lib/create-file');
-
-currentPath('a1/b1/c1/d1')
-
-
